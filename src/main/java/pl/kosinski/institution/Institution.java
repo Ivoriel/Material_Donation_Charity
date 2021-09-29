c8bd90a02ef04a5c582313110975619f11cfeef1
Ivoriel <tkivor@gmail.com>
2021-09-29 15:51:03 +0200

    Institution entity created.

---
+++ src/main/java/pl/kosinski/institution/Institution.java
@@ -0,0 +1,25 @@
+package pl.kosinski.institution;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Institution {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotNull
+    private String name;
+    @NotNull
+    private String description;
+
+}
