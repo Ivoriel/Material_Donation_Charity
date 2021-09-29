45c3f872711769c374b6a07cc8fd1faead1a4408
Ivoriel <tkivor@gmail.com>
2021-09-29 16:32:24 +0200

    Implementation for Institution service.

---
+++ src/main/java/pl/kosinski/institution/InstitutionViewAdapter.java
@@ -0,0 +1,56 @@
+package pl.kosinski.institution;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class InstitutionViewAdapter implements InstitutionService {
+
+    InstitutionRepository institutionRepository;
+
+    @Override
+    public InstitutionDto saveInstitution(InstitutionDto institutionDto) {
+        Institution institution = new Institution();
+        if (institutionDto.getId() != null) {
+            institution = institutionRepository.findById(institutionDto.getId()).get();
+            institution.setNameAndDescription(institutionDto.getName(), institutionDto.getDescription());
+            institution = institutionRepository.save(institution);
+        } else {
+            institution.setNameAndDescription(institutionDto.getName(), institutionDto.getDescription());
+            institution = institutionRepository.save(institution);
+        }
+        return mapEntityToDto(institution);
+    }
+
+    @Override
+    public InstitutionDto readInstitution(long id) {
+        return mapEntityToDto(institutionRepository.findById(id).get());
+    }
+
+    @Override
+    public List<InstitutionDto> findAll() {
+        List<Institution> institutionList = institutionRepository.findAll();
+        List<InstitutionDto> institutionDtoList = new ArrayList<>();
+        for (Institution institution : institutionList) {
+            institutionDtoList.add(mapEntityToDto(institution));
+        }
+        return institutionDtoList;
+    }
+
+    @Override
+    public void deleteInstitution(long id) {
+        institutionRepository.deleteById(id);
+    }
+
+    private InstitutionDto mapEntityToDto(Institution institution) {
+        InstitutionDto institutionDto = new InstitutionDto();
+        institutionDto.setId(institution.getId());
+        institutionDto.setName(institution.getName());
+        institutionDto.setDescription(institution.getDescription());
+        return institutionDto;
+    }
+}
