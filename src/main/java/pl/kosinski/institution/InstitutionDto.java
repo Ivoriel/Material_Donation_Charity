0b408b353685555af7b626f0fbb2901f0fb2de7f
Ivoriel <tkivor@gmail.com>
2021-09-29 15:53:24 +0200

    Institution DTO created.

---
+++ src/main/java/pl/kosinski/institution/InstitutionDto.java
@@ -0,0 +1,12 @@
+package pl.kosinski.institution;
+
+import lombok.Data;
+
+@Data
+public class InstitutionDto {
+
+    private Long id;
+    private String name;
+    private String description;
+
+}
