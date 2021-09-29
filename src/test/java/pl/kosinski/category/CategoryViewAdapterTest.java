6f2d1b8a277c7464c91e5aa19b743b98e54fa46c
Ivoriel <tkivor@gmail.com>
2021-09-29 15:40:46 +0200

    Created unit tests for Category view adapter.

---
+++ src/test/java/pl/kosinski/category/CategoryViewAdapterTest.java
@@ -0,0 +1,70 @@
+package pl.kosinski.category;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+public class CategoryViewAdapterTest {
+
+//    CategoryViewAdapter adapter;
+    @Autowired
+    CategoryRepository categoryRepository;
+
+    @Test
+    public void saveCategory() {
+        //Tests both save and read methods
+        CategoryViewAdapter adapter = new CategoryViewAdapter(categoryRepository);
+        CategoryDto category = new CategoryDto();
+        category.setName("test_category");
+        adapter.saveCategory(category);
+        category.setId(1L);
+        assertEquals(category, adapter.readCategory(1));
+    }
+
+    @Test
+    public void findAllCategories() {
+        CategoryViewAdapter adapter = new CategoryViewAdapter(categoryRepository);
+        List<CategoryDto> categoryList = new ArrayList<>();
+        CategoryDto category1 = new CategoryDto();
+        category1.setName("test_1");
+        adapter.saveCategory(category1);
+        category1.setId(1L);
+        categoryList.add(category1);
+        CategoryDto category2 = new CategoryDto();
+        category2.setName("test_2");
+        adapter.saveCategory(category2);
+        category2.setId(2L);
+        categoryList.add(category2);
+        assertEquals(categoryList, adapter.findAllCategories());
+    }
+
+    @Test
+    public void deleteCategory() {
+        CategoryViewAdapter adapter = new CategoryViewAdapter(categoryRepository);
+        CategoryDto category1 = new CategoryDto();
+        category1.setName("test_1");
+        adapter.saveCategory(category1);
+        category1.setId(1L);
+        CategoryDto category2 = new CategoryDto();
+        category2.setName("test_2");
+        adapter.saveCategory(category2);
+        category2.setId(2L);
+        assertEquals(category1, adapter.readCategory(1));
+        adapter.deleteCategory(1);
+        assertEquals(category2, adapter.readCategory(2));
+        assertThrows(NoSuchElementException.class, () -> adapter.readCategory(category1.getId()));
+    }
+}
