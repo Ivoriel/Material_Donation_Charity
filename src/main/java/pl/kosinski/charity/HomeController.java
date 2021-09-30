f3eb33f48d71de5ba00a905504e39a0349cbd504
Ivoriel <tkivor@gmail.com>
2021-09-30 16:28:27 +0200

    Added list containing double institution objects to model.

--- src/main/java/pl/kosinski/charity/HomeController.java
+++ src/main/java/pl/kosinski/charity/HomeController.java
@@ -1,17 +1,22 @@
 package pl.kosinski.charity;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.kosinski.institution.InstitutionService;
 
 
 @Controller
 @RequestMapping("/charity")
+@AllArgsConstructor
 public class HomeController {
 
+    InstitutionService institutionService;
 
     @RequestMapping("")
     public String homeAction(Model model){
+        model.addAttribute("doubleInstitutions", institutionService.getDoubleInstitutions());
         return "index";
     }
 }
