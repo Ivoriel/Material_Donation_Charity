cc6ebb0024c2c22d9747fce14532fa86be9ea192
Ivoriel <tkivor@gmail.com>
2021-10-06 19:11:45 +0200

    LocalDate converter for proper date persistence.

---
+++ src/main/java/pl/kosinski/donation/LocalDateConverter.java
@@ -0,0 +1,15 @@
+package pl.kosinski.donation;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+
+@Component
+public class LocalDateConverter implements Converter<String, LocalDate> {
+
+    @Override
+    public LocalDate convert(String source) {
+        return LocalDate.parse(source);
+    }
+}
