8d15cee8d709a2999f7fb90dc19e035a97c762ff
Ivoriel <tkivor@gmail.com>
2021-10-21 10:53:42 +0200

    User entity added.

---
+++ src/main/java/pl/kosinski/user/User.java
@@ -0,0 +1,30 @@
+package pl.kosinski.user;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotNull
+    private String email;
+    @NotNull
+    private String password;
+
+    public void saveEmailAndPassword(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+}
