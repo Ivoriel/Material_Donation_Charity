6912a539138a577a567326228f1ba836d6df6ddd
Ivoriel <tkivor@gmail.com>
2021-10-26 17:13:39 +0200

    User view adapter save method added.

--- src/main/java/pl/kosinski/user/UserViewAdapter.java
+++ src/main/java/pl/kosinski/user/UserViewAdapter.java
@@ -1,5 +1,6 @@
 package pl.kosinski.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -7,9 +8,25 @@ import java.util.List;
 @Service
 public class UserViewAdapter implements UserService{
 
+    @Autowired
+    UserRepository userRepository;
+
     @Override
     public UserDto saveUser(UserDto userDto) {
-        return null;
+        var user = new User();
+        if (userDto.getId() != null) {
+            user = getUser(userDto.getId());
+            user.saveEmailAndPassword(userDto.getEmail(), userDto.getPassword());
+        } else {
+            user.saveEmailAndPassword(userDto.getEmail(), userDto.getPassword());
+        }
+        user = userRepository.save(user);
+        userDto.setId(user.getId());
+        return userDto;
+    }
+
+    private User getUser(long id) {
+        return userRepository.getOne(id);
     }
 
     @Override
