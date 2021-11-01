05ac2c1b9aea5f5f1a2e78433ed07bfa81f98d50
Ivoriel <tkivor@gmail.com>
2021-11-01 08:37:59 +0100

    Created User unit tests class

---
+++ src/test/java/pl/kosinski/user/UserViewAdapterTest.java
@@ -0,0 +1,28 @@
+package pl.kosinski.user;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UserViewAdapterTest {
+
+    @Test
+    public void saveUser() {
+        //To do
+    }
+
+    @Test
+    public void findUser() {
+        //To do
+    }
+
+    @Test
+    public void findAllUsers() {
+        //To do
+    }
+
+    @Test
+    public void deleteUser() {
+        //To do
+    }
+}
