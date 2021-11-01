186d46e0c14c10c930da53d39ac1cb38e09005b2
Ivoriel <tkivor@gmail.com>
2021-11-02 00:03:09 +0100

    Missing constructor added.

--- src/main/java/pl/kosinski/user/UserViewAdapter.java
+++ src/main/java/pl/kosinski/user/UserViewAdapter.java
@@ -1,5 +1,6 @@
 package pl.kosinski.user;
 
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -7,6 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@AllArgsConstructor
 public class UserViewAdapter implements UserService{
 
     @Autowired
