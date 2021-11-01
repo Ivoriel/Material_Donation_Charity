4ea3610b01362b0bdc4ec931344e8c097015f8cc
Ivoriel <tkivor@gmail.com>
2021-11-01 23:52:22 +0100

    Created User unit tests configuration.

--- src/test/java/pl/kosinski/user/UserViewAdapterTest.java
+++ src/test/java/pl/kosinski/user/UserViewAdapterTest.java
@@ -1,10 +1,21 @@
 package pl.kosinski.user;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.*;
 
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class UserViewAdapterTest {
+
+    @Autowired
+    UserRepository userRepository;
 
     @Test
     public void saveUser() {
