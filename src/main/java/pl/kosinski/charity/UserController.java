26dcced2835a949783d350d136a1a12e1453e99d
Ivoriel <tkivor@gmail.com>
2021-11-05 09:53:14 +0100

    User controller created.

---
+++ src/main/java/pl/kosinski/charity/UserController.java
@@ -0,0 +1,15 @@
+package pl.kosinski.charity;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import pl.kosinski.user.UserService;
+
+@Controller
+@RequestMapping("user")
+@AllArgsConstructor
+public class UserController {
+
+    UserService userService;
+
+}
