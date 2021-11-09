f54f428a55dfe97a9dd051caaa014d41d27b4a0b
Ivoriel <tkivor@gmail.com>
2021-11-09 09:19:05 +0100

    UderDto added to model.

--- src/main/java/pl/kosinski/charity/UserController.java
+++ src/main/java/pl/kosinski/charity/UserController.java
@@ -2,8 +2,10 @@ package pl.kosinski.charity;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.kosinski.user.UserDto;
 import pl.kosinski.user.UserService;
 
 @Controller
@@ -14,7 +16,8 @@ public class UserController {
     UserService userService;
 
     @GetMapping("/register")
-    public String registerUser() {
+    public String registerUser(Model model) {
+        model.addAttribute(new UserDto());
         return "register";
     }
 
