bc6f9f24373945d5b4263e5fc923e5005d5d680e
Ivoriel <tkivor@gmail.com>
2021-11-12 00:30:57 +0100

    Registration post method added to the controller.

--- src/main/java/pl/kosinski/charity/UserController.java
+++ src/main/java/pl/kosinski/charity/UserController.java
@@ -3,10 +3,15 @@ package pl.kosinski.charity;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.kosinski.user.UserDto;
 import pl.kosinski.user.UserService;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/user")
@@ -19,6 +24,15 @@ public class UserController {
     public String registerUser(Model model) {
         model.addAttribute("user", new UserDto());
         return "/user/register";
+    }
+
+    @PostMapping("/register")
+    public String registerUser (@Valid @ModelAttribute("user") UserDto user, BindingResult result) {
+        if (result.hasErrors()) {
+            return "/user/register";
+        }
+        userService.saveUser(user);
+        return "user registered";
     }
 
 }
