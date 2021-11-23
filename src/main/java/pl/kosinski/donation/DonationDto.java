91d73a1cbb6a1684c078b2f0167241e320355557
Ivoriel <tkivor@gmail.com>
2021-11-24 00:09:47 +0100

    Added "Not blank" annotation to establish requirement for the appripriate fields.

--- src/main/java/pl/kosinski/donation/DonationDto.java
+++ src/main/java/pl/kosinski/donation/DonationDto.java
@@ -4,6 +4,7 @@ import lombok.Data;
 import pl.kosinski.category.Category;
 import pl.kosinski.institution.Institution;
 
+import javax.validation.constraints.NotBlank;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ArrayList;
@@ -13,13 +14,21 @@ import java.util.List;
 public class DonationDto {
 
     private Long id;
+    @NotBlank
     private int quantity;
+    @NotBlank
     private List<Category> categories = new ArrayList<>();
+    @NotBlank
     private Institution institution;
+    @NotBlank
     private String street;
+    @NotBlank
     private String city;
+    @NotBlank
     private String zipCode;
+    @NotBlank
     private LocalDate pickUpDate;
+    @NotBlank
     private LocalTime pickUpTime;
     private String pickUpComment;
 
