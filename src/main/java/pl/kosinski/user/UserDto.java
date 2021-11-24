73ac9325bbdf2790883198758f419f20a9fc48ef
Ivoriel <tkivor@gmail.com>
2021-11-25 00:14:29 +0100

    Added "Not blank" annotation to establish requirement for the appripriate fields.

--- src/main/java/pl/kosinski/user/UserDto.java
+++ src/main/java/pl/kosinski/user/UserDto.java
@@ -2,11 +2,15 @@ package pl.kosinski.user;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+
 @Data
 public class UserDto {
 
     private Long id;
+    @NotBlank
     private String email;
+    @NotBlank
     private String password;
 
 }
