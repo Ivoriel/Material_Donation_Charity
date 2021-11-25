158e71adff14490ed86d4cc3e3c4a7308309bfb2
Ivoriel <tkivor@gmail.com>
2021-11-26 00:17:16 +0100

    Imports organization.

--- src/main/java/pl/kosinski/charity/UserController.java
+++ src/main/java/pl/kosinski/charity/UserController.java
@@ -4,10 +4,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import pl.kosinski.user.UserDto;
 import pl.kosinski.user.UserService;
 
