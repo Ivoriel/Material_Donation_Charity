f5956a66dc200a882d9ee606ef0332c904e710da
Ivoriel <tkivor@gmail.com>
2021-12-18 23:28:29 +0100

    Login filter added.

--- src/main/java/pl/kosinski/charity/LoginFilter.java
+++ src/main/java/pl/kosinski/charity/LoginFilter.java
@@ -1,2 +1,14 @@
-package pl.kosinski.charity;public class LoginFilter {
+package pl.kosinski.charity;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+@Component
+public class LoginFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+    }
 }
