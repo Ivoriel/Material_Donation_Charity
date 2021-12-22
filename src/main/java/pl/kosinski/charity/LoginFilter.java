f7b6d4d58ff09f5ecdfbf0feb8a957aa9863af1c
Ivoriel <tkivor@gmail.com>
2021-12-23 00:07:30 +0100

    Login silter suspended

--- src/main/java/pl/kosinski/charity/LoginFilter.java
+++ src/main/java/pl/kosinski/charity/LoginFilter.java
@@ -3,12 +3,23 @@ package pl.kosinski.charity;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@Component
-public class LoginFilter implements Filter {
-    @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-
-    }
-}
+//@Component
+//public class LoginFilter implements Filter {
+//    @Override
+//    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+//
+////        HttpSession session = ((HttpServletRequest) request).getSession();
+////        Boolean userLoggedIn = false;
+////        if (session.getAttribute("userLoggedIn") != null) {
+////            userLoggedIn = (Boolean) session.getAttribute("userLoggedIn");
+////        }
+////        if (!userLoggedIn) {
+////            ((HttpServletResponse) response).sendRedirect("/charity/login");
+////        }
+//    }
+//}
