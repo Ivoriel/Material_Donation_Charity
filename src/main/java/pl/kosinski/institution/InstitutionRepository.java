e76c6d7bac39ce336139c4dccde9bbf7aa78da97
Ivoriel <tkivor@gmail.com>
2022-01-08 14:47:53 +0100

    Institution repository added.

--- src/main/java/pl/kosinski/institution/InstitutionRepository.java
+++ src/main/java/pl/kosinski/institution/InstitutionRepository.java
@@ -2,5 +2,10 @@ package pl.kosinski.institution;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface InstitutionRepository extends JpaRepository <Institution, Long> {
+
+    Institution findByName(String name);
+
 }
