3ee7ce82a86aba31141fb98d452b88a97a1c9ecc
Ivoriel <tkivor@gmail.com>
2022-01-17 08:52:34 +0100

    Fixed model loading for proper error displaying.

--- src/main/java/pl/kosinski/charity/InstitutionController.java
+++ src/main/java/pl/kosinski/charity/InstitutionController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.kosinski.institution.InstitutionDto;
@@ -26,7 +27,7 @@ public class InstitutionController {
     }
 
     @PostMapping("/add")
-    public String addInstitution(@Valid InstitutionDto institution, BindingResult result, Model model){
+    public String addInstitution(@Valid @ModelAttribute ("institution") InstitutionDto institution, BindingResult result, Model model){
         if (result.hasErrors()) {
             return "/institution/add";
         } else if (institutionService.IsInstitutionDuplicate(institution.getName())) {
