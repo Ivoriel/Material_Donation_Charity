3bd0144d77906ec7f2b7112f08c3b4059cd28695
Ivoriel <tkivor@gmail.com>
2022-02-03 07:12:52 +0100

    Test names refactor.

--- src/test/java/pl/kosinski/user/UserViewAdapterTest.java
+++ src/test/java/pl/kosinski/user/UserViewAdapterTest.java
@@ -23,16 +23,21 @@ public class UserViewAdapterTest {
     UserRepository userRepository;
 
     @Test
-    public void saveUser() {
+    public void checkIfUserIsSavedAndRetrievedWhenCalled() {
+        // Tests both save and read methods
+        // given Category not present in db
         var userViewAdapter = new UserViewAdapter(userRepository);
         var user = generateUser();
+        // when User is saved to db
         userViewAdapter.saveUser(user);
         user.setId(1L);
+        // then User should be retrievable when called by id
         assertEquals(user, userViewAdapter.findUser(1));
     }
 
     @Test
-    public void findAllUsers() {
+    public void checkIfListOfUsersPresentInDbCanBeRetrieved() {
+        // given Users are present in db
         var userViewAdapter = new UserViewAdapter(userRepository);
         var userList = new ArrayList<UserDto>();
         var user1 = generateUser();
@@ -43,37 +48,48 @@ public class UserViewAdapterTest {
         userViewAdapter.saveUser(user2);
         user2.setId(2L);
         userList.add(user2);
+        // when call to find all Users is executed
+        // then all Users should be retrieved
         assertEquals(userList, userViewAdapter.findAllUsers());
     }
 
     @Test
-    public void deleteUser() {
+    public void checkIfUserPresentInDbIsDeleted() {
+        // given User is present in db
         var userViewAdapter = new UserViewAdapter(userRepository);
         var user = generateUser();
         userViewAdapter.saveUser(user);
         user.setId(1L);
         assertEquals(user, userViewAdapter.findUser(1));
+        // when User is deleted from db
         userViewAdapter.deleteUser(1);
+        // then User should not be retrievable
         assertThrows(JpaObjectRetrievalFailureException.class, () -> userViewAdapter.findUser(1));
     }
 
     @Test
-    public void findUserByEmail() {
+    public void checkIfUserIsRetrievedWhenCalledByEmail() {
+        //given User is present in db
         var userViewAdapter = new UserViewAdapter(userRepository);
         var user = generateUser();
         userViewAdapter.saveUser(user);
         user.setId(1L);
         user.setPassword(userViewAdapter.findUser(1).getPassword());
+        // when call to find User by email is executed
+        // then Institution of specific email should be retrieved
         assertEquals(user, userViewAdapter.findUserByEmail(user.getEmail()));
     }
 
     @Test
-    public void checkPassword() {
+    public void checkUserPasswordWhenCalled() {
+        //given User is present in db
         var userViewAdapter = new UserViewAdapter(userRepository);
         var user = generateUser();
         userViewAdapter.saveUser(user);
         user.setId(1L);
         var savedUser = userViewAdapter.findUser(1);
+        // when call to check specific User's password is executed
+        // then true or false should be returned
         assertEquals(true, userViewAdapter.verifyPassword(user.getPassword(), savedUser));
     }
 
