3399822876613eb4dec21e5293441c436c4c9ec9
Ivoriel <tkivor@gmail.com>
2022-02-05 10:58:55 +0100

    Login filter in progress

--- src/main/java/pl/kosinski/charity/LoginFilter.java
+++ src/main/java/pl/kosinski/charity/LoginFilter.java
@@ -14,13 +14,15 @@ public class LoginFilter implements Filter {
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
 
         HttpSession session = ((HttpServletRequest) request).getSession();
-        Boolean userLoggedIn;
-        if (session.getAttribute("userLoggedIn") == null) {
+        var userLoggedIn = session.getAttribute("userLoggedIn");
+        if (userLoggedIn == null) {
             session.setAttribute("userLoggedIn", false);
+            userLoggedIn = false;
         }
         filterChain.doFilter(request, response);
-//        if (!userLoggedIn) {
+//        if (! (boolean) userLoggedIn) {
 //            ((HttpServletResponse) response).sendRedirect("/charity/login");
+//            break;
 //        }
     }
 }
