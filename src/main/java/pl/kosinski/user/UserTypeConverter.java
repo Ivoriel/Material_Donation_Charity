15cca9afcdc3fbe596c534f7ea5efe06ef50f181
Ivoriel <tkivor@gmail.com>
2022-02-10 01:15:52 +0100

    User type enum converter created.

---
+++ src/main/java/pl/kosinski/user/UserTypeConverter.java
@@ -0,0 +1,15 @@
+package pl.kosinski.user;
+
+import org.springframework.core.convert.converter.Converter;
+
+public class UserTypeConverter implements Converter<String, UserType> {
+
+    public UserType convert (String source) {
+        for (UserType uT : UserType.values()) {
+            if (uT.getLabel().equalsIgnoreCase(source)) {
+                return uT;
+            }
+        }
+        return null;
+    }
+}
