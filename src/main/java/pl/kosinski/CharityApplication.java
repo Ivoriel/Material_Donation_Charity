bb19a277ab351efdc059645e653898367797fee6
Ivoriel <tkivor@gmail.com>
2022-03-23 08:49:59 +0100

    Login filter bean registered in application.

--- src/main/java/pl/kosinski/CharityApplication.java
+++ src/main/java/pl/kosinski/CharityApplication.java
@@ -2,12 +2,37 @@ package pl.kosinski;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import pl.kosinski.charity.LoginFilter;
 
+import java.util.logging.Filter;
+
+//@Configuration
 @SpringBootApplication
 public class CharityApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CharityApplication.class, args);
     }
+
+    @Bean
+    public FilterRegistrationBean<LoginFilter> loginFilterRegistration(){
+        FilterRegistrationBean<LoginFilter> registrationBean
+                = new FilterRegistrationBean<>();
+
+        registrationBean.setFilter(new LoginFilter());
+        registrationBean.addUrlPatterns("/institution/*");
+        registrationBean.addUrlPatterns("/user/profile/*");
+        registrationBean.addUrlPatterns("/user/list/*");
+        registrationBean.setOrder(1);
+
+        return registrationBean;
+    }
+
+//    public LoginFilter loginFilter () {
+//        return new LoginFilter();
+//    }
 
 }
